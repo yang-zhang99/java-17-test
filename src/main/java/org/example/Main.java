d0126db5d161efed686fdfe2d2140d0c98b3a868
yang99 <yang.zhang99@outlook.com>
2023-02-14 10:41:10 +0800

    1124. 表现良好的最长时间段

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,13 @@
 package org.example;
 
-import java.io.FileInputStream;
-import java.net.URLEncoder;
-
 public class Main {
     public static void main(String[] args) {
 
-        EqualSubstring equalSubstring = new EqualSubstring();
-        System.out.println(equalSubstring.equalSubstring("abcd", "bdcf", 3));
+//        EqualSubstring equalSubstring = new EqualSubstring();
+//        System.out.println(equalSubstring.equalSubstring("abcd", "bdcf", 3));
 
-
+        LongestWPI longestWPI = new LongestWPI();
+        longestWPI.longestWPI(new int[]{9, 9, 6, 0, 6, 6, 9});
 
 
     }
