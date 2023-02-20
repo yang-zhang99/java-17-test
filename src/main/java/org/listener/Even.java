dae61736d6979bca3b23dda8e5d5a6125d6d5627
yang99 <yang.zhang99@outlook.com>
2023-02-20 09:09:18 +0800

    观察者模式

---
+++ src/main/java/org/listener/Even.java
@@ -0,0 +1,27 @@
+package org.listener;
+
+
+/**
+ * 事件对象
+ */
+public class Even {
+
+    private Robot robot;
+
+    public Even(){
+        super();
+    }
+    public Even(Robot robot){
+        super();
+        this.robot = robot;
+    }
+
+
+    public Robot getRobot() {
+        return robot;
+    }
+
+    public void setRobot(Robot robot) {
+        this.robot = robot;
+    }
+}
